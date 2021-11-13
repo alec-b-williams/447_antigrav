af7fc034f70d2cb45457a25219d3b2746ee44b4b
Ryan Welborn <ryan_welborn@wsu.edu>
2021-11-12 18:02:04 -0800

    movement moved into vehicle
    better structured too

--- src/gravity/Vehicle.java
+++ src/gravity/Vehicle.java
@@ -5,8 +5,7 @@ import jig.ResourceManager;
 import jig.Vector;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
-
-import static java.lang.Math.abs;
+import org.newdawn.slick.Input;
 
 public class Vehicle extends Entity {
     Animation sprite;
@@ -14,9 +13,7 @@ public class Vehicle extends Entity {
     public float worldY;
     private Vector speed;
     private double speedAngle;
-    private int turnCooldown;
     private boolean backUp;
-    //private float angle = 0;
 
     private static final float degPerSecond = 180;
 
@@ -32,45 +29,65 @@ public class Vehicle extends Entity {
         sprite.setCurrentFrame(5);
         speed = new Vector(0, 0);
         speedAngle = 180;
-        //turnCooldown = 1000;
         backUp = false;
     }
 
-    public void update(GameContainer container, GravGame gg, int delta){
-
+    public void update(GameContainer container, int delta){
+        Input input = container.getInput();
+        if(input.isKeyDown(Input.KEY_W)){
+            if(this.backUp && this.speed.length() > 0){
+                this.finishMovement(-1, 0.6f, 0.2f * 0.01f);
+            }
+            this.linearMovement(1, 0.06f, 0.2f, 1.1f);
+        }
+        if(input.isKeyDown(Input.KEY_S)){
+            if(!this.backUp && this.speed.length() > 0){
+                this.finishMovement(1, 0.6f, 0.2f * 0.01f);
+            }
+            this.linearMovement(-1, 0.01f, 0.05f, 1.05f);
+        }
+        if(input.isKeyDown(Input.KEY_A)){
+            this.turn(1, delta);
+        }
+        if(input.isKeyDown(Input.KEY_D)){
+            this.turn(-1, delta);
+        }
+        if(!input.isKeyDown(Input.KEY_W) && !input.isKeyDown(Input.KEY_S)){
+            if(this.backUp && this.speed.length() > 0){
+                this.finishMovement(-1, 0.9f, 0.05f * 0.05f);
+            }
+            else if(!this.backUp && this.speed.length() > 0){
+                this.finishMovement(1, 0.8f, 0.2f * 0.01f);
+            }
+        }
     }
-    public boolean getBackUp(){
-        return this.backUp;
+
+    public void finishMovement(int dir, float slowdownScale, float stopThreshold){
+        this.setSpeed(this.speed.scale(slowdownScale));
+        if(this.speed.length() < stopThreshold){
+            this.setSpeed(new Vector(0, 0));
+            this.backUp = !this.backUp;
+        }
+        this.worldY += dir * this.speed.getY();
+        this.worldX += dir * this.speed.getX();
     }
 
-    public void setBackUp(boolean value){
-        this.backUp = value;
-    }
-
-    public int getTurnCooldown(){
-        return this.turnCooldown;
-    }
-
-    public void setTurnCooldown(int delta){
-        this.turnCooldown += delta;
-    }
-
-    public void resetCooldown(){
-        this.turnCooldown = 0;
-    }
-
-    public Vector getSpeed(){
-        return speed;
+    public void linearMovement(int dir, float initLen, float speedLimit, float speedScale){
+        if(this.speed.length() == 0){
+            this.setSpeed(Vector.getVector(this.speedAngle, initLen));
+        }
+        else if(this.speed.length() < speedLimit){
+            this.setSpeed(this.speed.scale(speedScale));
+        }
+        if(this.speed.length() > speedLimit){
+            this.setSpeed(Vector.getVector(this.speedAngle, speedLimit));
+        }
+        this.worldY += dir * this.speed.getY();
+        this.worldX += dir * this.speed.getX();
     }
 
     public void setSpeed(Vector speed){
         this.speed = speed;
-    }
-
-    public void setSpeedRotation(double theta){
-
-        this.speed = this.speed.rotate(theta);
-        this.speedAngle = this.speed.getRotation();
     }
 
     public void turn(int dir, int delta) {
@@ -84,13 +101,4 @@ public class Vehicle extends Entity {
         System.out.println("Angle: " + newAngle + ", index: " + index);
         this.sprite.setCurrentFrame(index);
     }
-
-    public void initSpeedAngle(double theta){
-        this.speedAngle = theta;
-    }
-
-    public double getSpeedAngle(){
-        return this.speedAngle;
-    }
-
 }
